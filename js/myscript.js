01c55fba45ea0bcad7927a136d9480e2942b95d9
miura <miura@qu-bic.jp>
2020-11-09 19:09:52 +0900

    トップページ：作成中（pc、tab）

--- js/myscript.js
+++ js/myscript.js
@@ -18,9 +18,43 @@ const tabletwidth = 1025;
 
 $(function () {
 	$("body").removeClass("preload");
-// JQueryの範囲
+	// JQueryの範囲
 
+	$(window).on('load resize', function () {
+		var swiper01, swiper02, swiper03, swiper04, swiper05, swiper06, swiper07, swiper08, swiperMain;
 
+		swiper01 = new Swiper('.top04 .item01 .slider .swiper-container', {
+			loop: true,
+			speed: 1000,
+			slidesPerView: 1,
+			centeredSlides : true,
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: true
+			},
+			pagination: {
+				el: '.top04 .item01 .slider .swiper-pagination',
+				type: 'bullets',
+				clickable: true
+			}
+		});
+
+		swiper02 = new Swiper('.top04 .item02 .slider .swiper-container', {
+			loop: true,
+			speed: 1000,
+			slidesPerView: 1,
+			centeredSlides : true,
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: true
+			},
+			pagination: {
+				el: '.top04 .item02 .slider .swiper-pagination',
+				type: 'bullets',
+				clickable: true
+			}
+		});
+	});
 
 
 
